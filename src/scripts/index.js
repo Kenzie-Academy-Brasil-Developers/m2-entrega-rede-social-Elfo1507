e86ddbe2ad9289f0754548736fa0bfb92f3bc2f0
Elfo <alefisantos15@gmail.com>
2022-09-05 10:06:49 -0300

    feat: adiciona css, adiciona posts

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -2,6 +2,24 @@ import { Api } from "./models/api.js";
 
 const btnLogin = document.querySelector("#btn__logar")
 const btnIrProCadastro = document.querySelector("#ir__cadastro")
+const btnIrProCadastro2 = document.querySelector(".ir__cadastro")
+const btnIrProLogin = document.querySelector(".ir__Login")
+
+Api.conferenciaLoginPage()
+
+class Login{
+    static estiloBtn(){
+        const estiloBtnLoginClasse = document.querySelector(".ir__Login") 
+        const estiloBtnCadastroClasse = document.querySelector(".ir__cadastro")
+        if(window.location.href == "../../index.html"){
+            console.log("foi")
+            estiloBtnLoginClasse.classList.remove("btnForaPagina")
+            estiloBtnCadastroClasse.classList.add("btnForaPagina")
+        }
+    }
+}
+
+Login.estiloBtn()
 
 btnLogin.addEventListener('click', (event) => {
     event.preventDefault()
@@ -12,3 +30,12 @@ btnIrProCadastro.addEventListener('click', (event) => {
     event.preventDefault()
     window.location.href = "../../src/pages/cadastro.html"
 })
+btnIrProCadastro2.addEventListener('click', (event) => {
+    event.preventDefault()
+    window.location.href = "../../src/pages/cadastro.html"
+})
+btnIrProLogin.addEventListener('click', (event) => {
+    event.preventDefault()
+    window.location.reload()
+})
+
--- src/scripts/models/cadastro.js
+++ src/scripts/models/cadastro.js
@@ -2,6 +2,21 @@ import { Api } from "./api.js";
 
 const btnCadastro = document.querySelector("#btn__cadastrar")
 const btnIrProLogin = document.querySelector("#ir__cadastro")
+const btnIrProLogin2 = document.querySelector(".ir__Login")
+const btnIrProCadastro = document.querySelector(".ir__cadastro")
+
+class Cadastro{
+    static estiloBtn(){
+        const estiloBtnLoginClasse = document.querySelector(".ir__Login") 
+        const estiloBtnCadastroClasse = document.querySelector(".ir__cadastro")
+        if(window.location.href == "../../../src/pages/cadastro.html"){
+            estiloBtnLoginClasse.classList.add("btnForaPagina")
+            estiloBtnCadastroClasse.classList.remove("btnForaPagina")
+        }
+    }
+}
+
+Cadastro.estiloBtn()
 
 btnCadastro.addEventListener('click', (event) => {
     event.preventDefault()
@@ -11,4 +26,12 @@ btnCadastro.addEventListener('click', (event) => {
 btnIrProLogin.addEventListener('click', (event) => {
     event.preventDefault()
     window.location.href = "../../../index.html"
+})
+btnIrProLogin2.addEventListener('click', (event) => {
+    event.preventDefault()
+    window.location.href = "../../../index.html"
+})
+btnIrProCadastro.addEventListener('click', (event) => {
+    event.preventDefault()
+    window.location.reload()
 })
